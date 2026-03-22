dbd6081f0e8458edbbe35004b81d59e301049582
Patrick Souza <patricksouza1989@gmail.com>
2026-03-22 06:36:33 -0300

    Refazendo conexao limpa para o Render

--- backend-python/app/database.py
+++ backend-python/app/database.py
@@ -2,9 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-# ATENÇÃO AQUI: Troque "SUA_SENHA_NOVA_AQUI" pela senha que você criou.
-# Não coloque colchetes [ ], nem aspas, nem espaços. Fica assim: ...postgres:12345678@db...
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres.kryojhxwmlkufzavjmgj:Pa13be11so.89@aws-0-sa-east-1.pooler.supabase.com:5432/postgres?sslmode=require"
+# Cole a sua URL perfeita aqui (com psycopg2, sua senha e o sslmode no final)
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres.SEU_PROJETO:Pa13be11so.89@SEU_POOLER.pooler.supabase.com:5432/postgres?sslmode=require"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
