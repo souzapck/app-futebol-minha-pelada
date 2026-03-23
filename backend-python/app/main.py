3df8602a81c3473b71e823cc193a4da103f1eb67
Patrick Souza <patricksouza1989@gmail.com>
2026-03-23 19:04:22 -0300

    fix CORS Vercel frontend

--- backend-python/app/main.py
+++ backend-python/app/main.py
@@ -12,7 +12,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["http://localhost:5173", 
+                   "http://localhost:3000" , 
+                   "https://minha-pelada-futquinta.vercel.app"
+                   ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
