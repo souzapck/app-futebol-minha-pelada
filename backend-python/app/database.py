8762f88a90bf315518e67924de9a0f96b650d2eb
Patrick Souza <patricksouza1989@gmail.com>
2026-03-24 13:43:23 -0300

    fix: conexão banco corrigida

--- backend-python/app/database.py
+++ backend-python/app/database.py
@@ -3,9 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-if not DATABASE_URL:
-    DATABASE_URL = "postgresql+psycopg2://postgres:Pa13be11so.89@db.kryojhxwmlkufzavjmgj.supabase.co:5432/postgres?sslmode=require"
+DATABASE_URL = "postgresql+psycopg2://postgres:Pa13be11so.89@db.kryojhxwmlkufzavjmgj.supabase.co:5432/postgres?sslmode=require"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
