42c1dac6d3bf0755f1c21547c36103ba82eb1e67
souzapck <patricksouza1989@gmail.com>
2026-03-27 00:10:56 -0300

    Update BallVotePage.jsx

--- frontend/src/pages/BallVotePage.jsx
+++ frontend/src/pages/BallVotePage.jsx
@@ -154,7 +154,7 @@ export default function BallVotePage({ user }) {
     if (!matchDate) return null;
 
     const start = new Date(`${matchDate}T22:10:00-03:00`);
-    const end = new Date(`${matchDate}T01:00:00-03:00`);
+    const end = new Date(`${matchDate}T00:20:00-03:00`);
   end.setDate(end.getDate() + 1);
 
     return { start, end };
